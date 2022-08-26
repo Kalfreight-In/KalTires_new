f3ee356026ab6f22afef7c798f18b65b90c9b70c
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-08-26 22:03:44 +0530

    changes in lol

--- Components/upperbar/TopUpbar.jsx
+++ Components/upperbar/TopUpbar.jsx
@@ -3,23 +3,32 @@ import Link from 'next/link';
 // import Image from 'next/image';
 
 import { GoLocation } from 'react-icons/go';
+import { useStateContext } from '../../context/StateContext';
 
-const TopUpbar = () => (
-  <div className="bg-tire-gray-3 ">
-    <div className="flexBetween z-10 mx-20 p-3 flex-row">
-      <Link href="/">
-        <div className="flex flex-row flexCenter">
-          <div className="text-white font-bold ml-8">Indianapolis, IN</div>
-          <div className="ml-2 font-bold">
-            <GoLocation color="white " />
+const TopUpbar = () => {
+  const { location } = useStateContext();
+  return (
+    <div className="bg-tire-gray-3 ">
+      <div className="flexBetween z-10 mx-20 p-3 flex-row">
+        <Link href="/">
+          <div className="flex flex-row flexCenter">
+            <div className="text-white font-bold ml-8">
+              {' '}
+              {location.loaded
+                ? JSON.stringify(location)
+                : 'Location data not available yet.'}
+            </div>
+            <div className="ml-2 font-bold">
+              <GoLocation color="white " />
+            </div>
           </div>
-        </div>
-      </Link>
-      <Link href="/">
-        <div className="text-white">Help</div>
-      </Link>
+        </Link>
+        <Link href="/">
+          <div className="text-white">Help</div>
+        </Link>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default TopUpbar;
