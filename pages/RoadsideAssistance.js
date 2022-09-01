52816ffbbda2e37218bfaf7e65ea9d64df88921c
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-09-01 20:54:12 +0530

    changes in lol

--- pages/RoadsideAssistance.js
+++ pages/RoadsideAssistance.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import HeroSection from '../Components/Home/HeroSection';
 import Emergencyassist from '../Components/Roadside_Assitance/Emergencyassist';
 import Onsiteservice from '../Components/Roadside_Assitance/Onsiteservice';
 import Roadside from '../Components/Roadside_Assitance/Roadside24.jsx';
 
+const data = [
+  {
+    id: 1,
+    img: 'https://raw.githubusercontent.com/shantanusoam/kal_tires/kaltireBranch/Assets/Images/Home/HomePageSlider-1.png',
+    Heading: 'TRANSFORMING TRUCKING DYNAMICS',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique arcu a nisi, semper. Ut cras odio ac sem ac.',
+
+    CTA1: { heading: 'Inventory', link: 'contactus' },
+    CTA12: { heading: 'Inventory', link: 'contactus' },
+  },
+];
+
 const RoadsideAssistance = () => (
   <>
+    <HeroSection data={data[0]} />
     <Roadside />
     <Emergencyassist />
     <Onsiteservice />
--- pages/WhatWeOffer.js
+++ pages/WhatWeOffer.js
@@ -4,9 +4,23 @@ import { Services } from '../Components/What_We_Offer/Services';
 import { IndustTires } from '../Components/What_We_Offer/IndustTires';
 import CommercialTire from '../Components/What_We_Offer/CommercialTire';
 import Contactform from '../Components/What_We_Offer/ContactForm';
+import HeroSection from '../Components/Home/HeroSection';
+
+const data = [
+  {
+    id: 1,
+    img: 'https://raw.githubusercontent.com/shantanusoam/kal_tires/kaltireBranch/Assets/Images/Home/HomePageSlider-1.png',
+    Heading: 'TRANSFORMING TRUCKING DYNAMICS',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique arcu a nisi, semper. Ut cras odio ac sem ac.',
+
+    CTA1: { heading: 'Inventory', link: 'contactus' },
+    CTA12: { heading: 'Inventory', link: 'contactus' },
+  },
+];
 
 const WhatWeOffer = () => (
   <div>
+    <HeroSection data={data[0]} />
     <CommercialTire />
     <Speciality />
     <IndustTires />
