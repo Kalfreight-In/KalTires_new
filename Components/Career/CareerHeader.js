6008f332edeaa7dbc2e33ed637d9620d05210bb9
Kd <umarkhurshid3@gmail.com>
2022-09-07 05:01:32 +0530

    update in careerHeader and deparments

--- Components/Career/CareerHeader.js
+++ Components/Career/CareerHeader.js
@@ -8,12 +8,12 @@ export const CareerHeader = () => {
   const isMobile = useMediaQuery('(max-width:768px)');
 
   return (
-    <div id="mainimagecareerheader">
-      <Image
-        src={isMobile ? headerbannermobile : headerimagecareer}
-        alt="headerImage"
-        objectFit="cover"
-      />
-    </div>
+    // <div id="mainimagecareerheader">
+    <Image
+      src={isMobile ? headerbannermobile : headerimagecareer}
+      alt="headerImage"
+      objectFit="contain"
+    />
+    // </div>
   );
 };
