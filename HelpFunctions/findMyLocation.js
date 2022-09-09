d26a7e3ab05f8ec30115ce6e6b9b54301ca7823d
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-09-10 01:14:04 +0530

    changes

--- HelpFunctions/findMyLocation.js
+++ HelpFunctions/findMyLocation.js
@@ -7,3 +7,26 @@ export const findMyLocation = (setUserLocation) => {
     });
   }
 };
+function getLocation() {
+  console.log('getLocation was called');
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, positionError);
+  } else {
+    hideLoadingDiv();
+    console.log('Geolocation is not supported by this device');
+  }
+}
+
+function positionError() {
+  console.log('Geolocation is not enabled. Please enable to use this feature');
+
+  if (allowGeoRecall && countLocationAttempts < 5) {
+    countLocationAttempts += 1;
+    getLocation();
+  }
+}
+
+function showPosition() {
+  console.log('posititon accepted');
+  allowGeoRecall = false;
+}
