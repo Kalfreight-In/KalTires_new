7b77e63eef6152870d603b8febc29468fa977c1f
Kd <umarkhurshid3@gmail.com>
2022-09-13 04:18:53 +0530

    update in logos commercialtire

--- data/data.js
+++ data/data.js
@@ -411,7 +411,7 @@ export const OnsiteGriddata = [
   {
     id: 4,
     img: onsiteeight,
-    heading: 'Friendly & Experienced Technicians',
+    heading: 'Freindly & Experience Technicians',
     desc: 'With a customer-centric approach, our committed team of technicians works diligently to offer quick & hassle-free services. ',
   },
   // {
@@ -1866,7 +1866,7 @@ export const MapData2 = [
 
 export const careerKvl = [
   {
-    Headingm: 'Careers at KVL Tires',
+    Headingm: 'CAREERS AT KVL TIRES',
     desc: 'At KVL Tires, we build strong relationships with everyone. People everywhere entrust us with the care of their vehicles. We drive on the mission of ‘Efficiency on the move’. From tires to tuning up your vehicle, we ensure it runs at its best. The relentless pursuit of our goals extends beyond moving forward. It contributes to creating a workplace that celebrates the diversity of skills, capabilities, and experiences. Hence, inspiring us to grow, develop expertise, and encourage the right career moves. We stay committed to building a better future for employees, clients, and others.  ',
   },
 ];
