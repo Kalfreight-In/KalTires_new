50d855f47325849261d8dc6758289d4ac1eefada
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-09-16 03:52:50 +0530

    change

--- Components/Home/AboutUs.jsx
+++ Components/Home/AboutUs.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import { MdLocalPhone } from 'react-icons/md';
 // import { BiMessageDetail } from 'react-icons/bi';
+import { motion } from 'framer-motion';
 import AboutUsTire from '../../Assets/Images/Home/AboutUsTire.png';
 import AgriculturalTires from '../../Assets/Images/Home/AgriculturalTires.png';
 import CommercialTire from '../../Assets/Images/Home/CommercialTires.png';
@@ -11,6 +12,7 @@ import useMediaQuery from '../../Hooks/CustomMediaQuery';
 export const AboutUs = () => {
   const [ShowMoreAboutUS, setShowMoreAboutUS] = useState(false);
   const isDesktop = useMediaQuery('(min-width:1148px)');
+
   return (
     <div id="AboutUs_Section">
       <div
@@ -44,75 +46,88 @@ export const AboutUs = () => {
                 //   className="py-12 md:px-10 px-6 lg:px-0 max-w-3xl lg:max-w-md mx-auto col-span-2 "
                 className="  inline-block     mx-auto   "
               >
-                <h2 className="text-4xl tracking-tight   font-poppins font-semibold lg:inline-block hidden">
-                  <span className=" text-neutral-800  border-b-2 hover:border-b-4 hover:pb-0 transition-all ease-in-out -pb-2 duration-100  border-y-PrimaryTwo   2xl:w-1/2 xl:w-3/5 w-4/5">
-                    ABOUT US
-                  </span>
-                </h2>
-                <div className="text-neutral-500 mt-5 2xl:text-lg lg:text-base text-sm  font-medium text-justify font-Helvetica">
-                  Are you looking for quality tires for your fleet? Does a
-                  blowout leave you stranded on your way? Are you looking for
-                  24/7 roadside assistance? Are you short of time and unable to
-                  find pick-up & delivery services nearby? Don’t worry; we have
-                  it all covered! KAL Group Inc. has launched a full-service
-                  commercial tire business vertical, KVL Tires.{' '}
-                  {isDesktop ? (
-                    <>
-                      <br />
-                      <br />
-                    </>
-                  ) : null}
-                  KVL Tires is a full-service commercial tire dealer based in
-                  the United States. With our headquarters in Fontana,
-                  California, we are currently operating in 18+ locations across
-                  the US and growing. KVL Tires offers high-quality products and
-                  services to a diverse clientele at affordable prices.
-                  <br />
-                  <p
-                    className={`text-justify mt-4 font-Helvetica Transition-Height-${
-                      ShowMoreAboutUS ? 'in' : 'out'
-                    }`}
-                  >
-                    {ShowMoreAboutUS ? (
-                      <p>
-                        Our unparalleled range of new, used & retread tires
-                        caters to the needs of all commercial vehicles. The
-                        extensive tire product lines include commercial,
-                        agricultural, off-the-road (OTR), industrial, and
-                        earthmover tires. We have partnered with trusted tire
-                        manufacturers such as Continental, Goodyear, BKT,
-                        General, Ameristeel, Tornado, Cooper, and many more
+                <motion.div
+                  initial={{ y: +50, opacity: 0.5 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  <h2 className="text-4xl tracking-tight   font-poppins font-semibold lg:inline-block hidden">
+                    <span className=" text-neutral-800  border-b-2 hover:border-b-4 hover:pb-0 transition-all ease-in-out -pb-2 duration-100  border-y-PrimaryTwo   2xl:w-1/2 xl:w-3/5 w-4/5">
+                      ABOUT US
+                    </span>
+                  </h2>
+                </motion.div>
+                <motion.div
+                  initial={{ y: +50, opacity: 0.5 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.3 }}
+                >
+                  <div className="text-neutral-500 mt-5 2xl:text-lg lg:text-base text-sm  font-medium text-justify font-Helvetica">
+                    Are you looking for quality tires for your fleet? Does a
+                    blowout leave you stranded on your way? Are you looking for
+                    24/7 roadside assistance? Are you short of time and unable
+                    to find pick-up & delivery services nearby? Don’t worry; we
+                    have it all covered! KAL Group Inc. has launched a
+                    full-service commercial tire business vertical, KVL Tires.{' '}
+                    {isDesktop ? (
+                      <>
                         <br />
                         <br />
-                        Besides selling new tires, KVL Tires also offers 24/7
-                        Roadside Assistance Service. You can quickly find
-                        skilled technicians nearby, even in remote areas. Our
-                        services include pick-up & delivery, on-site services,
-                        flat repairs, mounted wheel programs, wheel alignment
-                        services, light mechanical services, and more.
-                        <br />
-                        <br />
-                        At KVL Tires, everybody is welcome, whether an owner
-                        operator, repair shop or fleet owner. Our trained
-                        specialists strive to deliver quality, safety, and value
-                        for a smooth tire buying & service experience. We put
-                        the time into understanding our customer’s needs and
-                        providing them with satisfactory solutions. We continue
-                        to invest in our service facilities, employees,
-                        equipment, vehicles, and technology systems to provide
-                        you with top-notch tires & services. So, if you need
-                        ‘passenger car tires’ or ‘tire alignment near me’, reach
-                        out to us without a second thought.
-                      </p>
+                      </>
                     ) : null}
-                  </p>
-                  <div
-                    onClick={() => setShowMoreAboutUS(!ShowMoreAboutUS)}
-                    className="underline decoration-2 decoration-sky-500 text-PrimaryTwo font-medium cursor-pointer "
-                  >
-                    {!ShowMoreAboutUS ? 'Show More' : 'Show less'}
+                    KVL Tires is a full-service commercial tire dealer based in
+                    the United States. With our headquarters in Fontana,
+                    California, we are currently operating in 18+ locations
+                    across the US and growing. KVL Tires offers high-quality
+                    products and services to a diverse clientele at affordable
+                    prices.
+                    <br />
+                    <p
+                      className={`text-justify mt-4 font-Helvetica Transition-Height-${
+                        ShowMoreAboutUS ? 'in' : 'out'
+                      }`}
+                    >
+                      {ShowMoreAboutUS ? (
+                        <p>
+                          Our unparalleled range of new, used & retread tires
+                          caters to the needs of all commercial vehicles. The
+                          extensive tire product lines include commercial,
+                          agricultural, off-the-road (OTR), industrial, and
+                          earthmover tires. We have partnered with trusted tire
+                          manufacturers such as Continental, Goodyear, BKT,
+                          General, Ameristeel, Tornado, Cooper, and many more
+                          <br />
+                          <br />
+                          Besides selling new tires, KVL Tires also offers 24/7
+                          Roadside Assistance Service. You can quickly find
+                          skilled technicians nearby, even in remote areas. Our
+                          services include pick-up & delivery, on-site services,
+                          flat repairs, mounted wheel programs, wheel alignment
+                          services, light mechanical services, and more.
+                          <br />
+                          <br />
+                          At KVL Tires, everybody is welcome, whether an owner
+                          operator, repair shop or fleet owner. Our trained
+                          specialists strive to deliver quality, safety, and
+                          value for a smooth tire buying & service experience.
+                          We put the time into understanding our customer’s
+                          needs and providing them with satisfactory solutions.
+                          We continue to invest in our service facilities,
+                          employees, equipment, vehicles, and technology systems
+                          to provide you with top-notch tires & services. So, if
+                          you need ‘passenger car tires’ or ‘tire alignment near
+                          me’, reach out to us without a second thought.
+                        </p>
+                      ) : null}
+                    </p>
+                    <div
+                      onClick={() => setShowMoreAboutUS(!ShowMoreAboutUS)}
+                      className="underline decoration-2 decoration-sky-500 text-PrimaryTwo font-medium cursor-pointer "
+                    >
+                      {!ShowMoreAboutUS ? 'Show More' : 'Show less'}
+                    </div>
                   </div>
-                </div>
+                </motion.div>
               </div>
               <div className="block h-auto">
                 <div>
