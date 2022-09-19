2ab7e7b555f3cf26c340eb0abb789706f44981d4
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-09-19 22:01:03 +0530

    fsdf

--- Components/Home/RoadSideAssistant.jsx
+++ Components/Home/RoadSideAssistant.jsx
@@ -51,7 +51,7 @@ const RoadSideAssistant = () => {
                 ROADSIDE ASSISTANCE
               </h2>
 
-              <p className="text-neutral-500 mt-5 2xl:text-lg pr-4 overflow-y-scroll lg:text-base text-sm font-normal text-left font-Helvetica scrollbar">
+              <p className="text-neutral-500 mt-5 2xl:text-lg pr-4 overflow-y-scroll lg:text-base text-sm font-normal text-left font-Helvetica ">
                 Driving a commercial vehicle keeps you on the move, making it
                 necessary to be aware of your vehicle’s condition. You must
                 ensure that your tires deliver great mileage, superior traction,
