2666ccecba73038a7ab37c39eaf02ab62398e85b
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-09-19 21:27:32 +0530

    Roadside Assistance Now?

--- Components/Roadside_Assitance/Onsiteservice.jsx
+++ Components/Roadside_Assitance/Onsiteservice.jsx
@@ -18,7 +18,7 @@ const Onsiteservice = () => {
           <div>
             <div
               id="grid"
-              className="grid gap-x-4  bg-bg-white-1   grid-cols-onsiteTemplate pt-20 px-4 2xl:px-16"
+              className="grid gap-x-4  bg-bg-white-1   grid-cols-onsiteTemplate pt-20 px-20"
             >
               {OnsiteGriddata.map((index) => (
                 <div className="bg-bg-white-1 text-center" key={index.id}>
