31dd24561f5fb18a3d0b853ae2ddf11b641db69b
Kd <umarkhurshid3@gmail.com>
2022-09-21 22:52:28 +0530

    update in tailwind roadside ans service data js

--- Components/Whatweoffer/Services.jsx
+++ Components/Whatweoffer/Services.jsx
@@ -114,7 +114,9 @@ export const Services = () => {
                         />
                       </div>
                     </div>
-                    <h1 className="lg:text-xl text-lg font-bold font-Helvetica h-16">
+                    <h1
+                      className={`lg:text-xl text-lg font-bold font-poppins xl:px-8 md:px-0 whitespace-${data.linebrk} h-16`}
+                    >
                       {data.heading}
                     </h1>
                   </a>
