08e13b501a6a6115d250b533c9302419269c094b
Kd <umarkhurshid3@gmail.com>
2022-09-21 21:15:31 +0530

    update in perkbenefit

--- Components/Career/PerkBenifits.jsx
+++ Components/Career/PerkBenifits.jsx
@@ -21,7 +21,7 @@ export const PerkBenifits = () => {
             </h3>
             <div
               id="Perkgrid"
-              className="grid gap-4    bg-bg-white-1   grid-cols-PerkTemplate  pt-12 px-24"
+              className="grid gap-4    bg-bg-white-1   grid-cols-PerkTemplate  pt-12 px-20"
             >
               {PerkBenifitsdata.map((index) => (
                 <div
