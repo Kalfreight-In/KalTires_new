c8e2db5368392701772d36dcb1b3c950bea12d88
Kd <umarkhurshid3@gmail.com>
2022-09-23 01:58:21 +0530

    update in careerhehader

--- Components/Career/CareerHeader.js
+++ Components/Career/CareerHeader.js
@@ -9,9 +9,9 @@ const data = [
   {
     id: 1,
     img: 'https://raw.githubusercontent.com/Kalfreight-In/KalTires_new/main/Assets/Images/Career/headerbanner.png',
-    Heading: '',
-
-    // desc: 'WE HELP YOU ACHIEVE YOUR DREAMS',
+    Heading: 'Join Our Team',
+    // page: 'Careers'
+    // desc: 'Join Our Team',
 
     // CTA1: { heading: 'Apply Now', link: 'contactus' },
     // CTA12: { heading: 'Inventory', link: 'contactus' },
@@ -19,6 +19,23 @@ const data = [
 ];
 
 export const CareerHeader = () => (
+  <div id="careerHeadermain">
+    <div
+      id="careerHeadertwo"
+      style={{
+        backgroundImage:
+          // eslint-disable-next-line operator-linebreak
+          'url(' +
+          'https://raw.githubusercontent.com/shantanusoam/kal_tires/kaltireBranch/Assets/Images/Whatweoffer/Industrialtires/industbackg.png' +
+          ')',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        height: '',
+      }}
+    >
+      <h1 className="text-center">Join Our Team</h1>
+    </div>
+  </div>
   // const isMobile = useMediaQuery('(max-width:768px)');
 
   // <Image
@@ -26,5 +43,6 @@ export const CareerHeader = () => (
   //   alt="headerImage"
   //   objectFit="contain"
   // />
-  <HeroSection data={data[0]} />
+
+  // <HeroSection />
 );
