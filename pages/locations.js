06a96a6c742d29d39e7063329f24d108fe3f3318
Kd <umarkhurshid3@gmail.com>
2022-09-27 03:40:26 +0530

    update in data js and location

--- pages/locations.js
+++ pages/locations.js
@@ -296,11 +296,9 @@ const location = () => {
                                     KVL Tires
                                   </div>
                                   <div className=" font-normal text-md">
-                                    {`${toTitleCase(
-                                      value.properties.Address
-                                    )}${', '}  ${value.properties.City}, ${
-                                      value.properties.State
-                                    }
+                                    {`${value.properties.Address}${', '}  ${
+                                      value.properties.City
+                                    }, ${value.properties.State}
                                     ${value.properties.ZipCode}`}
                                   </div>
                                   {value.properties.Manager ? (
