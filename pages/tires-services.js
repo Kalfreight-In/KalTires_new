2e254180ac8bbeebf0c6fd95a125b363d15964cb
Kd <umarkhurshid3@gmail.com>
2022-09-27 20:54:34 +0530

    tires and service banner

--- pages/tires-services.js
+++ pages/tires-services.js
@@ -13,7 +13,7 @@ const data = [
     id: 1,
     img: 'https://raw.githubusercontent.com/Kalfreight-In/KalTires_new/main/Assets/Images/Whatweoffer/WhatWeOfferBg.png',
     Heading: 'Get back on the road safely with KVL Tires  ',
-    desc: 'Offering Major Tire Brands, Quality Tire Repair Services across 18+ Locations & Growing.  ',
+    desc: 'Offering Major Tire Brands, Quality Tire Repair Services across 18+ Locations & Growing ',
     CTA1: { heading: 'Contact Us', link: '/#maincontactform' },
     CTA12: { heading: 'Inventory', link: 'contactus' },
     page: 'tire',
