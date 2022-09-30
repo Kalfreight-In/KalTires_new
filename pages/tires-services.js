13300202d4c0f6a3e1d10fddbf7de3eba0854f44
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-10-01 01:22:28 +0530

    changes in contact us

--- pages/tires-services.js
+++ pages/tires-services.js
@@ -16,7 +16,7 @@ const data = [
     desc: 'Offering Major Tire Brands, Quality Tire Repair Services across 18+ Locations & Growing ',
     CTA1: {
       heading: 'Contact Us',
-      Slink: '/#maincontactform',
+      Slink: '/#CanadaContact',
       link: '/#CanadaContact',
     },
     CTA12: { heading: 'Inventory', link: 'contactus' },
