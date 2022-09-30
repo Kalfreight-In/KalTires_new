b720628a65cc5552e103893774f17b676dc13c71
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-10-01 00:34:02 +0530

    changes in live

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -642,14 +642,14 @@ const Footer = () => {
                         <MdLocationPin className=" h-6 w-5 " />
                       </a>
                     </div>
-                    <div className="ml-4">
+                    <div className="block  text-center">
                       <a
                         href="https://www.google.co.in/maps/place/10156+Live+Oak+Ave,+Fontana,+CA+92335,+USA/@34.0687994,-117.4834404,17z/data=!3m1!4b1!4m5!3m4!1s0x80c34b3fc6e50489:0xde8aefe027f7319!8m2!3d34.068795!4d-117.4812517"
                         target="_blank"
                         rel="noreferrer"
                       >
                         {' '}
-                        <div className="md:text-2xl text-lg font-semibold w-56">
+                        <div className="md:text-2xl text-lg font-semibold w-64">
                           10156 Live Oak Ave Fontana, CA 92335
                         </div>{' '}
                       </a>
