e1391df6a0e81652481c27c107cd9ac1c6dc03fe
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-11-01 05:03:54 +0530

    changes

--- pages/CookiePopup.js
+++ pages/CookiePopup.js
@@ -35,7 +35,10 @@ const CookiePopup = () => {
 
           <div className="flex flex-row justify-between items-center">
             <div className=" text-2xl">
-              We are Taking Your data to kill u Pls accept our poisonous cokkies
+              We use cookies on our website to give you the most relevant
+              experience by remembering your preferences and repeat visits. By
+              clicking Accept, you consent to the use of ALL the cookies. Do not
+              sell my personal information.
             </div>
           </div>
           <div className="flex ">
