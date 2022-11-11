fec93ae6a4b7bd1183ba6a945d7973eff9a5d53b
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-11-11 20:37:23 +0530

    changes

--- pages/api/CaptchCheck.js
+++ pages/api/CaptchCheck.js
@@ -1,25 +1,25 @@
 const validateCaptcha = (response_key) => {
-  return new Promise((resolve, reject) => {
-    const secret_key = process.env.NEXT_PUBLIC_SECRET_KEY;
+  // return new Promise((resolve, reject) => {
+  //   const secret_key = process.env.NEXT_PUBLIC_SECRET_KEY;
 
-    const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${response_key}`;
+  //   const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${response_key}`;
 
-    fetch(url, {
-      method: 'post',
-    })
-      .then((response) => response.json())
-      .then((google_response) => {
-        if (google_response.success == true) {
-          resolve(true);
-        } else {
-          resolve(false);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        resolve(false);
-      });
-  });
+  //   fetch(url, {
+  //     method: 'post',
+  //   })
+  //     .then((response) => response.json())
+  //     .then((google_response) => {
+  //       if (google_response.success == true) {
+  //         resolve(true);
+  //       } else {
+  //         resolve(false);
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       resolve(false);
+  //     });
+  // });
 };
 
 // import fetch from "node-fetch";
@@ -110,40 +110,40 @@ const validateCaptcha = (response_key) => {
 
 // // });
 
-// export default async (req, res) => {
-//   if (req.method === 'POST') {
-//     const { token } = req.body;
-//     axios
-//       .post(
-//         `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.NEXT_PUBLIC_SECRET_KEY}&response=${token}`
-//       )
-//       .then((res) => [
-//         res.  res.status(200).json({`Human 👨 👩 ${token} ${process.env.NEXT_PUBLIC_SECRET_KEY}`})
-//       ])
-//       .catch((e) => {
-//         res.status(401);
-//         res.send({
-//           error: `User already exists ${e} --  ${req.body}`,
-//         });
-//       });
-//     //sends secret key and response token to google
-//     // try {
-//     //   axios.post(
-//     //     `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.NEXT_PUBLIC_SECRET_KEY}&response=${token}`
-//     //   );
-//     // } catch (e) {
-//     //   res.status(401);
-//     //   res.send({
-//     //     error: `User already exists ${email} -- ${password} ${req.body}`,
-//     //   });
-//     //   return;
-//     // }
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    const { token } = req.body;
+    axios
+      .post(
+        `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.NEXT_PUBLIC_SECRET_KEY}&response=${token}`
+      )
+      .then((res) => [
+        res.  res.status(200).json({`Human 👨 👩 ${token} ${process.env.NEXT_PUBLIC_SECRET_KEY}`})
+      ])
+      .catch((e) => {
+        res.status(401);
+        res.send({
+          error: `User already exists ${e} --  ${req.body}`,
+        });
+      });
+    //sends secret key and response token to google
+    // try {
+    //   axios.post(
+    //     `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.NEXT_PUBLIC_SECRET_KEY}&response=${token}`
+    //   );
+    // } catch (e) {
+    //   res.status(401);
+    //   res.send({
+    //     error: `User already exists ${email} -- ${password} ${req.body}`,
+    //   });
+    //   return;
+    // }
 
-//     //check response status and send back to the client-side
+    //check response status and send back to the client-side
 
-//     // Process a POST request
-//   } else {
-//     // Handle any other HTTP method
-//     res.status(200).json({ message: process.env.NEXT_PUBLIC_SECRET_KEY });
-//   }
-// };
+    // Process a POST request
+  } else {
+    // Handle any other HTTP method
+    res.status(200).json({ message: process.env.NEXT_PUBLIC_SECRET_KEY });
+  }
+};
