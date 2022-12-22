916462dcb470c0ab341f604691a5117d363f8637
Kd <umarkhurshid3@gmail.com>
2022-12-23 02:36:01 +0530

    update in the footer js

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -161,7 +161,7 @@ const Footer = () => {
                 <div className="text-white flex xl:justify-start justify-start mt-1 md:ml-0 ml-0 ">
                   <div className="">
                     <a
-                      href="tel: 800-808-0025"
+                      href="https://www.google.co.in/maps/place/10156+Live+Oak+Ave,+Fontana,+CA+92335,+USA/@34.0687994,-117.4834404,17z/data=!3m1!4b1!4m5!3m4!1s0x80c34b3fc6e50489:0xde8aefe027f7319!8m2!3d34.068795!4d-117.4812517"
                       target="_blank"
                       rel="noreferrer"
                     >
